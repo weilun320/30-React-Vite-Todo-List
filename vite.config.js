7267ab8b6bcfae23ed9db94e73ec1848e27181fd
Wei Lun <41337787+weilun320@users.noreply.github.com>
2023-12-11 15:23:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/30-Todo-List-React-Vite/"
+  base: "/30-React-Vite-Todo-List/"
 })
